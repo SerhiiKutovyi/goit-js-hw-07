f9d1356b69025c0e01fbc22b9595882c3aaf0f9d
SerhiiKutovyi <wagenrex@gmail.com>
2022-10-09 15:13:45 +0300

    JS- HV-7 over

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
-
 console.log(galleryItems);
+
+const refGallery = document.querySelector('.gallery');
+
+const newItems = galleryItems
+  .map(
+    item =>
+      `<div class="gallery__item">
+    <a class="gallery__link" href='${item.original}'>
+    <img
+        class="gallery__image"
+        src="${item.preview}"
+        data-source="${item.original}"
+        alt="${item.description}"
+    />
+    </a>
+    </div>`
+  )
+  .join('');
+
+refGallery.insertAdjacentHTML('afterbegin', newItems);
+
+refGallery.addEventListener('click', openItemsClick);
+
+function openItemsClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`);
+  instance.show();
+
+  refGallery.addEventListener('keydown', event => {
+    if (event.code === 'Escape') {
+      instance.close();
+    }
+  });
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,40 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+//* 1 Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи. Используй готовый код из первого задания.
 
+import { galleryItems } from './gallery-items.js';
 console.log(galleryItems);
+
+const refGallery = document.querySelector('.gallery');
+
+const newItems = galleryItems
+  .map(
+    item =>
+      `<li class="gallery__item">
+    <a class="lazyload"  href="${item.original}">
+    <img class="gallery__image" loading='lazy' src="${item.preview}" alt="${item.description}" />
+    </a>
+    </li>`
+  )
+  .join('');
+
+refGallery.innerHTML = newItems;
+console.log(refGallery);
+
+/* 2. Подключение скрипта и стилей библиотеки используя CDN сервис cdnjs. 
+  Необходимо добавить ссылки на два файла: 
+  simple - lightbox.min.js и simple - lightbox.min.css.*/
+
+/* 3. Инициализация библиотеки после того как элементы галереи созданы и добавлены в div.gallery.
+ Для этого ознакомься с документацией SimpleLightbox - в первую очередь секции «Usage» и «Markup».*/
+
+refGallery.addEventListener('click', openItemsClick);
+function openItemsClick(event) {
+  event.preventDefault();
+  /*Посмотри в документации секцию «Options» и добавь отображение подписей к изображениям из атрибута alt. 
+  Пусть подпись будет снизу и появляется через 250 миллисекунд после открытия изображения.*/
+  if (event.target !== event.currentTarget) {
+    const lightbox = new SimpleLightbox('.gallery a', {
+      captionsData: 'alt',
+      captionDelay: 250,
+    });
+  }
+}
